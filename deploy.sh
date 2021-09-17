95e158479911697bdf6105229e51308dae0fdbbc
Kylian Lavergne <inklay@outlook.fr>
2021-09-17 16:25:34 +0200

    [FIX]: some linter errors

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
+#!/bin/bash
 docker build --tag tcg_image .
-
 docker container stop tcg
-
 docker container rm tcg
-
 docker container run -d --name "tcg" -v /data/Pokemon-TCG:/app/data tcg_image
-
 docker logs tcg
